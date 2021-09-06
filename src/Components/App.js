de6efc75ac33b8559ca107cda07c22c04c0b01e4
Godswill Edet <gimmex1@gmail.com>
2021-09-06 19:57:22 +0100

    Organized basic navigation

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,30 +2,36 @@ import React from "react";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import AnsweredQuestionDetail from "./AnsweredQuestionDetail";
-import AnswerQuestion from "./AnswerQuestion";
 import Four0Four from "./Four0Four";
 import LeaderBoard from "./LeaderBoard";
-import LeaderCard from "./LeaderCard";
 import Nav from "./Nav";
 import NewQuestion from "./NewQuestion";
-import QuestionDetail from "./QuestionDetail";
-import QuestionListItem from "./QuestionListItem";
 import UnansweredQuestionDetail from "./UnansweredQuestionDetail";
+import { Route, Switch } from "react-router";
+
 const App = () => {
   return (
     <div>
-      <Login />
-      <Dashboard />
-      <AnsweredQuestionDetail />
-      <AnswerQuestion />
-      <Four0Four />
-      <LeaderBoard />
-      <LeaderCard />
       <Nav />
-      <NewQuestion />
-      <QuestionDetail />
-      <QuestionListItem />
-      <UnansweredQuestionDetail />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/new" render={() => <NewQuestion />} />
+        <Route exact path="/login" render={() => <Login />} />
+        <Route exact path="/leaderboard" render={() => <LeaderBoard />} />
+        <Route
+          exact
+          path="/question/:unqId"
+          render={() => <UnansweredQuestionDetail />}
+        />
+        <Route
+          exact
+          path="/question/:aqId"
+          render={() => <AnsweredQuestionDetail />}
+        />
+        <Route exact path="/leaderboard" render={() => <LeaderBoard />} />
+
+        <Route exact render={() => <Four0Four />} />
+      </Switch>
     </div>
   );
 };
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>
-      <h3>Hello from the Nav view</h3>
-    </div>
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/new">New Question</Link>
+      <Link to="/leaderboard">Leaderboard</Link>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+    </>
   );
 };
 
