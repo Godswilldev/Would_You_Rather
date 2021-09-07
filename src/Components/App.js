52cc610ec443ef6a5e082858a8b819457f366472
Godswill Edet <gimmex1@gmail.com>
2021-09-07 13:15:45 +0100

    Added basic routing

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Login from "./Login";
-import Dashboard from "./Dashboard";
-import AnsweredQuestionDetail from "./AnsweredQuestionDetail";
-import Four0Four from "./Four0Four";
-import LeaderBoard from "./LeaderBoard";
-import Nav from "./Nav";
-import NewQuestion from "./NewQuestion";
-import UnansweredQuestionDetail from "./UnansweredQuestionDetail";
+import Login from "./Login/Login";
+import Dashboard from "./Dashboard/Dashboard";
+import Four0Four from "./FourZeroFour/Four0Four";
+import LeaderBoard from "./LeaderBoard/LeaderBoard";
+import Nav from "./Nav/Nav";
+import NewQuestion from "./NewQuestion/NewQuestion";
+import Question from "./Question/Question";
 import { Route, Switch } from "react-router";
 
 const App = () => {
@@ -18,18 +17,8 @@ const App = () => {
         <Route exact path="/new" render={() => <NewQuestion />} />
         <Route exact path="/login" render={() => <Login />} />
         <Route exact path="/leaderboard" render={() => <LeaderBoard />} />
-        <Route
-          exact
-          path="/question/:unqId"
-          render={() => <UnansweredQuestionDetail />}
-        />
-        <Route
-          exact
-          path="/question/:aqId"
-          render={() => <AnsweredQuestionDetail />}
-        />
+        <Route exact path="/question/:unqId" render={() => <Question />} />
         <Route exact path="/leaderboard" render={() => <LeaderBoard />} />
-
         <Route exact render={() => <Four0Four />} />
       </Switch>
     </div>
---
+++ src/Components/Question/Question.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = () => {
+  return (
+    <div>
+      <h1>HEllo from the question page</h1>
+    </div>
+  );
+};
+
+export default Question;
