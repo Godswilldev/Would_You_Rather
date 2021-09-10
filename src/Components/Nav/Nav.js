3867586e72d18d4064ae191b311264fbfe7271cc
Godswill Edet <gimmex1@gmail.com>
2021-09-10 13:51:42 +0100

    Implemented showing logged in user info on the navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,15 +6,26 @@ import { removeUser } from "../../Redux/Actions/AuthedUser";
 const Nav = () => {
   const dispatch = useDispatch();
   const authedUser = useSelector(({ authedUserReducer }) => authedUserReducer);
+  const users = useSelector(({ usersReducer }) => usersReducer);
+  const username = Object.values(users).find((user) => user.id === authedUser);
+
   return (
     <>
       <Link to="/">Home</Link>
       <Link to="/new">New Question</Link>
       <Link to="/leaderboard">Leaderboard</Link>
       {authedUser && (
-        <Link to="/login">
-          <button onClick={() => dispatch(removeUser())}>Logout</button>
-        </Link>
+        <div style={{ display: "flex" }}>
+          <h3>{username.name}</h3>
+          <img
+            style={{ width: "5rem", height: "5rem", borderRadius: "100%" }}
+            src={username.avatarURL}
+            alt={username.name}
+          />
+          <Link to="/login">
+            <button onClick={() => dispatch(removeUser())}>Logout</button>
+          </Link>
+        </div>
       )}
     </>
   );
