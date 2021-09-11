45de70e12e4da89be695173b0e86547f02ef1bc1
Godswill Edet <gimmex1@gmail.com>
2021-09-11 23:06:42 +0100

    styled navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { removeUser } from "../../Redux/Actions/AuthedUser";
 
 const Nav = () => {
@@ -9,26 +10,45 @@ const Nav = () => {
   const users = useSelector(({ usersReducer }) => usersReducer);
   const username = Object.values(users).find((user) => user.id === authedUser);
 
+  const Links = styled.div`
+    a {
+      text-decoration: none;
+      font-size: 1.2rem;
+    }
+  `;
+
   return (
-    <>
+    <Links
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: "#aafafa",
+        height: "3rem",
+      }}
+    >
       <Link to="/dashboard">Home</Link>
       <Link to="/new">New Question</Link>
       <Link to="/leaderboard">Leaderboard</Link>
-
       {authedUser && (
-        <div style={{ display: "flex" }}>
-          <h3>{username.name}</h3>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
           <img
-            style={{ width: "5rem", height: "5rem", borderRadius: "100%" }}
+            style={{ width: "2rem", height: "2rem", borderRadius: "100%" }}
             src={username.avatarURL}
             alt={username.name}
           />
+          <h5>{username.name}</h5>
           <Link to="/login">
             <button onClick={() => dispatch(removeUser())}>Logout</button>
           </Link>
         </div>
       )}
-    </>
+    </Links>
   );
 };
 
