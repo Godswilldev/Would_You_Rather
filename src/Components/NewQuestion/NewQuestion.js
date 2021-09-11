a1eb1468c00e52a55a1255923641f542099f2871
Godswill Edet <gimmex1@gmail.com>
2021-09-11 14:00:31 +0100

    removed useHistory

--- src/Components/NewQuestion/NewQuestion.js
+++ src/Components/NewQuestion/NewQuestion.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router";
 import useInputState from "../../hooks/useInputState";
 import { handleAddQuestion } from "../../Redux/Actions/NewQuestion";
 
